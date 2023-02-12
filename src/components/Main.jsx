c21d4d38417d67a561f0edc818ef0829652b868b
Pepui <lujerkovic@gmail.com>
2023-02-12 07:09:45 +0100

    a

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -35,7 +35,7 @@ const Main = () => {
               <a href="https://www.linkedin.com/in/luka-jerković">
                 <FaLinkedin className="cursor-pointer" size={30} src="" />
               </a>
-              <a href="https://github.com/LukaJerkovic;">
+              <a href="https://github.com/LukaJerkovic">
                 <FaGithub className="cursor-pointer" size={30} src="" />
               </a>
             </div>
